4cc423351b0ba85c8da8045dfd161c2f827210a3
Avery Yang <averyyang2674@gmail.com>
2023-06-14 19:07:27 -0500

    remove comments

--- src/api/openWeatherApi.js
+++ src/api/openWeatherApi.js
@@ -1,12 +1,3 @@
-// city search
-// https://api.openweathermap.org/data/2.5/weather?q=minneapolis&appid=b601b84d0e7ce5864247288c5731f8ae
-
-// lat and lon
-// https://api.openweathermap.org/data/2.5/weather?lat=44.98&lon=-93.2638&appid=b601b84d0e7ce5864247288c5731f8ae&units=imperial
-
-// forecast
-// https://api.openweathermap.org/data/2.5/forecast?q=minneapolis&units=imperial&appid=b601b84d0e7ce5864247288c5731f8ae
-
 import { DateTime } from 'luxon';
 const OPEN_API_URL = 'https://api.openweathermap.org/data/2.5';
 const OPEN_API_KEY = import.meta.env.VITE_API_KEY;
